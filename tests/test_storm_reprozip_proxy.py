c54be3d0b6afe9ba00ceda72e5d433d58f1fc8ed
Phelps <phelpsmemo@gmail.com>
2021-12-13 02:57:25 -0300

    cleaning tests directory

--- tests/test_storm_reprozip_proxy.py
+++ tests/test_storm_reprozip_proxy.py
@@ -7,33 +7,3 @@
 # details.
 
 """Module tests."""
-
-from flask import Flask
-
-from storm_reprozip_proxy import StormReprozipProxy
-
-
-def test_version():
-    """Test version import."""
-    from storm_reprozip_proxy import __version__
-    assert __version__
-
-
-def test_init():
-    """Test extension initialization."""
-    app = Flask('testapp')
-    ext = StormReprozipProxy(app)
-    assert 'storm-reprozip-proxy' in app.extensions
-
-    app = Flask('testapp')
-    ext = StormReprozipProxy()
-    assert 'storm-reprozip-proxy' not in app.extensions
-    ext.init_app(app)
-    assert 'storm-reprozip-proxy' in app.extensions
-
-
-def test_view(base_client):
-    """Test view."""
-    res = base_client.get("/")
-    assert res.status_code == 200
-    assert 'Welcome to storm-reprozip-proxy' in str(res.data)
